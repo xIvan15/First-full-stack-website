77f878ccc6b835243173e346c0265426d10663c6
Ivan <142749346+xIvan15@users.noreply.github.com>
2025-07-03 11:07:21 -0500

    Delete backend directory

--- backend/core/serializer.py
+++
@@ -1,9 +0,0 @@
-from rest_framework import serializers
-from . models import *
-
-#=====================================================# This is for converting complex data from the browser to native python datatypes
-
-class ReactSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = React
-        fields = '__all__'
